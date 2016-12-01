4b5d03fc5bcddb7ccc72eaf99d34259f74feca0b
Stasinda <stasinda.nsk@mail.ru>
2016-12-01 21:56:13 +0700

    ДЗ к 03.12. Вывод символов кодировки ASCII.

---
+++ AllSymbolsASCII/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace AllSymbolASCII
+{
+    class Program
+    {
+        static void Main()
+        {
+            for (int i = 0; i < 128; i++)
+            {
+                char ascii = Convert.ToChar(i);
+                // Символы char являются элементами Unicode
+                Console.WriteLine("Символ " + ascii + " в числовом виде равен " + i);
+                // ASCII - это подмножество Unicode, состоящее из 128 символов
+            }
+            Console.Read();
+        }
+    }
+}
